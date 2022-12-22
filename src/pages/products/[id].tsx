c3bb000f9f9f0b598faa5593d21a0408a884fe9d
iamjhpark <91qkrwjdgns@naver.com>
2022-12-22 17:34:41 +0900

    Initial Commit

---
+++ src/pages/products/[id].tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ProductDetail = () => <div>상세페이지</div>;
+
+export default ProductDetail;
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ProductList = () => <div>상품목록</div>;
+
+export default ProductList;
